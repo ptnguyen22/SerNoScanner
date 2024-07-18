3f70c9e5317e3744fae0d6d89adcb4eddefa0b4e
Peter Nguyen <ptnguyen422@gmail.com>
2024-07-18 09:34:58 -0400

    Initial Commit

---
+++ SerNoScanner/SerNoScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerNoScannerApp.swift
+//  SerNoScanner
+//
+//  Created by Peter Nguyen on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SerNoScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
